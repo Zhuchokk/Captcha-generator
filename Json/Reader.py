c3f8f971a9ca7582848ab93bbc717a21f8e3cb1a
Artiom <azhuchok207@gmail.com>
2021-10-20 00:44:23 +0300

    Added json reader and settings.json

---
+++ Json/Reader.py
@@ -0,0 +1,11 @@
+import json
+
+
+class Reader:
+    def __init__(self, path_to_json):
+        self.path = path_to_json
+
+    def read(self):
+        with open(self.path, 'r', encoding='utf-8') as f:
+            data = json.load(fp=f)
+        return data
